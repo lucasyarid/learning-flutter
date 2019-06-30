0bea480c45e3c00dcb8847ec4b918589764d54b0
Lucas Yarid <lucas.yarid@gmail.com>
2019-06-30 19:50:49 -0300

    button to add products

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:learning_flutter/product_manager.dart';
+import './pages/home.dart';
 
-import './products.dart';
-
-main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager(),
-      ),
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.teal,
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
     );
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -2,22 +2,38 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
-  
-  Products(this.products);
+
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/images/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Container();
+    }
+    return productCards;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/images/food.jpg'),
-                    Text(element)
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    print('[Products Widget] build()');
+    return _buildProductList();
   }
 }
